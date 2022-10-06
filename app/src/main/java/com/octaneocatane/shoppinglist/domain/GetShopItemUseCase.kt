78628464d44e3c9835f2ad96c0514971a5aa8dba
Octane Silvio <ruslangibadullin70@gmail.com>
2022-10-06 14:35:48 +0500

    scope

--- app/src/main/java/com/octaneocatane/shoppinglist/domain/GetShopItemUseCase.kt
+++ app/src/main/java/com/octaneocatane/shoppinglist/domain/GetShopItemUseCase.kt
@@ -1,7 +1,7 @@
 package com.octaneocatane.shoppinglist.domain
 
 class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
-    fun getShopItem(shopItemId: Int): ShopItem {
+    suspend fun getShopItem(shopItemId: Int): ShopItem {
         return shopListRepository.getShopItem(shopItemId)
     }
 }
--- app/src/main/java/com/octaneocatane/shoppinglist/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/octaneocatane/shoppinglist/presentation/ShopItemViewModel.kt
@@ -1,23 +1,26 @@
 package com.octaneocatane.shoppinglist.presentation
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
 import com.octaneocatane.shoppinglist.data.ShopListRepositoryImpl
 import com.octaneocatane.shoppinglist.domain.AddShopItemUseCase
 import com.octaneocatane.shoppinglist.domain.EditShopItemUseCase
 import com.octaneocatane.shoppinglist.domain.GetShopItemUseCase
 import com.octaneocatane.shoppinglist.domain.ShopItem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 import java.lang.Exception
 
+class ShopItemViewModel(application: Application) : AndroidViewModel(application) {
 
-class ShopItemViewModel : ViewModel() {
-
-    private val repository = ShopListRepositoryImpl
+    private val repository = ShopListRepositoryImpl(application)
 
     private val getShopItemUseCase = GetShopItemUseCase(repository)
     private val addShopItemUseCase = AddShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
+
     private val _errorInputName = MutableLiveData<Boolean>()
     val errorInputName: LiveData<Boolean>
         get() = _errorInputName
@@ -35,8 +38,10 @@ class ShopItemViewModel : ViewModel() {
         get() = _shouldCloseScreen
 
     fun getShopItem(shopItemId: Int) {
-        val item = getShopItemUseCase.getShopItem(shopItemId)
-        _shopItem.value = item
+        viewModelScope.launch {
+            val item = getShopItemUseCase.getShopItem(shopItemId)
+            _shopItem.value = item
+        }
     }
 
     fun addShopItem(inputName: String?, inputCount: String?) {
@@ -45,7 +50,9 @@ class ShopItemViewModel : ViewModel() {
         val fieldsValid = validateInput(name, count)
         if (fieldsValid) {
             val shopItem = ShopItem(name, count, true)
-            addShopItemUseCase.addShopItem(shopItem)
+            viewModelScope.launch {
+                addShopItemUseCase.addShopItem(shopItem)
+            }
             finishWork()
         }
     }
@@ -56,9 +63,11 @@ class ShopItemViewModel : ViewModel() {
         val fieldsValid = validateInput(name, count)
         if (fieldsValid) {
             _shopItem.value?.let {
-                val item = it.copy(name = name, count = count)
-                editShopItemUseCase.editShopItem(item)
-                finishWork()
+                viewModelScope.launch {
+                    val item = it.copy(name = name, count = count)
+                    editShopItemUseCase.editShopItem(item)
+                    finishWork()
+                }
             }
         }
     }
