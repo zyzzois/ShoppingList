e70341058b795112be9d4e2bf1adf3fdf7f9abed
Octane Silvio <ruslangibadullin70@gmail.com>
2023-01-15 16:47:09 +0500

    commit

---
+++ data/src/main/java/com/octaneocatane/data/provider/MyClass.java
@@ -0,0 +1,7 @@
+package com.octaneocatane.data.provider;
+
+import android.content.UriMatcher;
+
+public class MyClass {
+    UriMatcher myMatcher = new UriMatcher()
+}
---
+++ data/src/main/java/com/octaneocatane/data/provider/ShopListProvider.kt
@@ -0,0 +1,54 @@
+package com.octaneocatane.data.provider
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.UriMatcher
+import android.database.Cursor
+import android.net.Uri
+import android.util.Log
+import java.lang.Math.abs
+
+class ShopListProvider: ContentProvider() {
+
+    //val uriMatcher = UriMatcher()
+
+    override fun onCreate(): Boolean {
+        return true
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?,
+    ): Cursor? {
+        Log.d("ShoppingListProvider", "query $uri")
+        return null
+    }
+
+    override fun getType(uri: Uri): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+    ): Int {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+
+    }
+}
